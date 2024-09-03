3195740da9c6ff6bad99a8aa1370a59137fcda44
Andrew Roddy <andrewroddy7@gmail.com>
2024-09-02 21:20:44 -0400

    Delete pong.cpp

--- Pong/pong.cpp
+++
@@ -1,9 +0,0 @@
-#include <windows.h>
-
-int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
-    // Create Window Class
-
-    // Register Class
-
-    // Create Window
-}
